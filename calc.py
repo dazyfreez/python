d76c20dee27d781ced846f3046263ccb952d30a2
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-13 23:10:32 +0200

    -

--- calc.py
+++ calc.py
@@ -2,3 +2,9 @@ print ("das ist ein rechner")
 print ("wollen sie addieren oder subrtahiren?")
 print ("1. addieren")
 print ("2. subtrahieren")
+if (input("") == "1"):
+    print ("welche zahl soll addiert werden?")
+    print (input("") + input(""))
+elif (input("") == "2"):
+    print ("welche zahl soll subtrahiert werden?")
+    print (input("") - input(""))
