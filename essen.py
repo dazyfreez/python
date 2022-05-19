e9cd4e31b98a444474cfd0b6c3cd03ad2a4341c7
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-19 22:41:37 +0200

    --

--- essen.py
+++ essen.py
@@ -1,3 +1,7 @@
 print("das ist ein essensplaner")
 essen =  ["spagetti", "burger", "lasagne", "bestellen", "eier"]
 x = int(input("für wie viel tage wollen sie planen"))
+i = 0
+while x>i:
+    print(essen)
+    i=i+1
