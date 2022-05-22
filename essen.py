d7bd3ab22f8914e522886ec3b2bfd03d1db16db1
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-22 16:41:59 +0200

    -

--- essen.py
+++ essen.py
@@ -5,6 +5,42 @@ i = 0
 while i < x:
     print(essen[i])
     i = i + 1
-
+print("wollen sie die ergebnisse in einer anderen reihenfolge anzeigen")
+print("ja oder nein")
+eingabe = input()
+if eingabe == "ja":
+    essen.reverse()
+    print(essen)
+else:
+    print(essen)
+print("wollen sie einen neuen eintrag hinzufügen")
+print("ja oder nein")
+eingabe = input()
+if eingabe == "ja":
+    neu = input("was möchten sie hinzufügen")
+    essen.append(neu)
+    print(essen)
+else:
+    print(essen)
+print("wollen sie einen eintrag löschen")
+print("ja oder nein")
+eingabe = input()
+if eingabe == "ja":
+    löschen = input("was möchten sie löschen")
+    essen.remove(löschen)
+    print(essen)
+else:
+    print(essen)
+print("wollen sie einen eintrag ändern")
+print("ja oder nein")
+eingabe = input()
+if eingabe == "ja":
+    ändern = input("was möchten sie ändern")
+    essen.remove(ändern)
+    neu = input("was möchten sie ändern")
+    essen.append(neu)
+    print(essen)
+else:
+    print(essen)
 
 
