05ac6631ad6c26b46bbc6fb0a6c05ce28452ebfd
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-23 18:12:54 +0200

    -

---
+++ binarysearch.py
@@ -0,0 +1,12 @@
+print("das ist ein binarysearch algorithmus")
+liste = [1,2,3,4,5,6,7,8,9,10]
+x = int(input("welche zahl möchten sie suchen"))
+i = 0
+while i < len(liste):
+    if liste[i] == x:
+        print("gefunden")
+        break
+    elif liste[i] > x:
+        print("nicht gefunden")
+        break
+    i = i + 1
