6ce00a9d832310d663d95aef2d3b9427d4112da0
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:05:32 +0200

    -

--- russischroulette.py
+++ russischroulette.py
@@ -6,4 +6,6 @@ eingabe = input()
 if eingabe == "ja":
     print("sie können zwischen 6 zahlen whälen")
     print("wollen sie das spiel starten bedenken sie ihr leben hängt an ihrer wahl ab")
-    print(random.randint(0, 6))
+    x = random.randint(1, 6)
+    print(x)
+    
