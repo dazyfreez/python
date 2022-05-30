af47447f0d49ccb2e4b54586121f5777f51c469b
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:16:13 +0200

    -

--- russischroulette.py
+++ russischroulette.py
@@ -92,6 +92,39 @@ if eingabe == "ja":
                                 print("wollen sie nochmal spielen")
                                 print("ja oder nein")
                                 eingabe = input()
+                                if eingabe == "ja":
+                                    print("sie können zwischen 1-6 whälen")
+                                    print("wollen sie das spiel starten bedenken sie ihr leben hängt an ihrer wahl ab")
+                                    y = int(input("wie viele zahlen wollen sie wählen"))
+                                    print(y)
+                                    x = random.randint(1,6)
+                                    print(x)
+                                    if x!=y:
+                                        print("sie leben noch")
+                                        print("wollen sie nochmal spielen")
+                                        print("ja oder nein")
+                                        eingabe = input()
+                                        if eingabe == "ja":
+                                            print("sie können zwischen 1-6 whälen")
+                                            print("wollen sie das spiel starten bedenken sie ihr leben hängt an ihrer wahl ab")
+                                            y = int(input("wie viele zahlen wollen sie wählen"))
+                                            print(y)
+                                            x = random.randint(1,6)
+                                            print(x)
+                                            if x!=y:
+                                                print("sie leben noch")
+                                                print("wollen sie nochmal spielen")
+                                                print("ja oder nein")
+                                                eingabe = input()
+                                                if eingabe == "ja":
+                                                    print("sie können zwischen 1-6 whälen")
+                                                    print("wollen sie das spiel starten bedenken sie ihr leben hängt an ihrer wahl ab")
+                                                    y = int(input("wie viele zahlen wollen sie wählen"))
+                                                    print(y)
+                                                    x = random.randint(1,6)
+                                                    print(x)
+                                                    if x!=y:
+                                                        print
     #if y == 6:
         #print("wählen sie 6 zahlen")
         #zahlen = [1,2,3,4,5,6]
