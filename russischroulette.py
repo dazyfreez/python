7c6f2d91b10f86c72a1bac79fe514cef8ce73490
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:03:36 +0200

    -

--- russischroulette.py
+++ russischroulette.py
@@ -1,4 +1,4 @@
-
+import random
 print("das ist ein RUSSISCHE ROULETTE SPIEL")  # hier wird das spiel gestartet
 print("wollen sie das spiel starten")       # hier wird das spiel gestartet
 print("ja oder nein")   
