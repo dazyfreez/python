0afa9a17267bfc656e3c5b55669f47de3b16e399
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:03:26 +0200

    -

--- russischroulette.py
+++ russischroulette.py
@@ -1,6 +1,7 @@
+
 print("das ist ein RUSSISCHE ROULETTE SPIEL")  # hier wird das spiel gestartet
 print("wollen sie das spiel starten")       # hier wird das spiel gestartet
 print("ja oder nein")   
 eingabe = input()
 if eingabe == "ja":
-    
+    print("sie können zwischen 6 zahlen whälen")
