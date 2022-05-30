ce25584a0656f443aa4e270775fa1735cae6d68d
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-30 23:10:21 +0200

    -#

--- russischroulette.py
+++ russischroulette.py
@@ -7,51 +7,53 @@ if eingabe == "ja":
     print("sie können zwischen 6 zahlen whälen")
     print("wollen sie das spiel starten bedenken sie ihr leben hängt an ihrer wahl ab")
     y = int(input("wie viele zahlen wollen sie wählen"))
-    if y == 6:
-        print("wählen sie 6 zahlen")
-        zahlen = [1,2,3,4,5,6]
-        print(zahlen)
-        x = int(input("welche zahl möchten sie suchen"))
-        i = 0
-        while i < len(zahlen):
-            if zahlen[i] == x:
-                print("gefunden")
-                break
-            elif zahlen[i] > x:
-                print("nicht gefunden")
-                break
-            i = i + 1
-    elif y == 5:
-        print("wählen sie 5 zahlen")
-        zahlen = [1,2,3,4,5]
-        print(zahlen)
-        x = int(input("welche zahl möchten sie suchen"))
-        i = 0
-        while i < len(zahlen):
-            if zahlen[i] == x:
-                print("gefunden")
-                break
-            elif zahlen[i] > x:
-                print("nicht gefunden")
-                break
-            i = i + 1
-    elif y == 4:
-        print("wählen sie 4 zahlen")
-        zahlen = [1,2,3,4]
-        print(zahlen)
-        x = int(input("welche zahl möchten sie suchen"))
-        i = 0
-        while i < len(zahlen):
-            if zahlen[i] == x:
-                print("gefunden")
-                break
-            elif zahlen[i] > x:
-                print("nicht gefunden")
-                break
-            i = i + 1
-    elif y == 3:
-        print("wählen sie 3 zahlen")
-        zahlen = [1,2,3]
-        print(zahlen)
-        x = int(input("welche zahl möchten sie suchen"))
-        i = 0
+    print(y)
+
+    #if y == 6:
+        #print("wählen sie 6 zahlen")
+        #zahlen = [1,2,3,4,5,6]
+        #print(zahlen)
+        #x = int(input("welche zahl möchten sie suchen"))
+       # i = 0
+       # while i < len(zahlen):
+        #    if zahlen[i] == x:
+             #   print("gefunden")
+                #break
+          #  elif zahlen[i] > x:
+          #      print("nicht gefunden")
+            #    break
+       #     i = i + 1
+   # elif y == 5:
+   #     print("wählen sie 5 zahlen")
+        #zahlen = [1,2,3,4,5]
+      #  print(zahlen)
+       # x = int(input("welche zahl möchten sie suchen"))
+       # i = 0
+       # while i < len(zahlen):
+      #     if zahlen[i] == x:
+         #       print("gefunden")
+            #    break
+         #   elif zahlen[i] > x:
+        #        print("nicht gefunden")
+           #     break
+     #      i = i + 1
+ #   elif y == 4:
+      #  print("wählen sie 4 zahlen")
+     #   zahlen = [1,2,3,4]
+      #  print(zahlen)
+       # x = int(input("welche zahl möchten sie suchen"))
+      #  i = 0
+     #   while i < len(zahlen):
+         #   if zahlen[i] == x:
+         #       print("gefunden")
+           #     break
+          #  elif zahlen[i] > x:
+         #       print("nicht gefunden")
+        #        break
+       #     i = i + 1
+    #elif y == 3:
+    #    print("wählen sie 3 zahlen")
+      #  zahlen = [1,2,3]
+      #  print(zahlen)
+      #  x = int(input("welche zahl möchten sie suchen"))
+      #  i = 0
