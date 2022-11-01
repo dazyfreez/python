d201079dd3e6ee7728d8471a62f3254612f1bdfa
dazyfreez <dazyfreez@gmail.com>
2022-11-02 00:43:26 +0100

    new def

--- sort.py
+++ sort.py
@@ -65,6 +65,24 @@ def sort_list_by_length_and_alphabetically_and_reverse():
     # now we will print the sorted list
     print("sorted list: " + str(list))
 
+def sort_list_by_length_and_alphabetically_and_reverse_and_print():
+    # now we will sort a list by the length of the strings
+    # and alphabetically
+    # we will use the sorted function
+    list = ["hello", "world", "this", "is", "a", "list", "of", "strings"]
+    print("unsorted list: " + str(list))
+    # now we will sort the list
+    list = sorted(list, key=len)
+    # now we will sort the list alphabetically
+    list = sorted(list)
+    # now we will reverse the list
+    list.reverse()
+    # now we will print the sorted list
+    print("sorted list: " + str(list))
+    # now we will print the list
+    for i in list:
+        print(i)
+
 print("welcome to the sorting algorithmus")
 print("please choose an option")
 print("1. sort a list")
@@ -72,7 +90,8 @@ print("2. sort a list by yourself")
 print("3. sort a list by the length of the strings")
 print("4. sort a list by the length of the strings and alphabetically")
 print("5. sort a list by the length of the strings and alphabetically and reverse")
-print("6. exit")
+print("6. sort a list by the length of the strings and alphabetically and reverse and print")
+print("7. exit")
 while True:
     x = int(input("please choose an option"))
     if x == 1:
@@ -86,6 +105,8 @@ while True:
     elif x == 5:
         sort_list_by_length_and_alphabetically_and_reverse()
     elif x == 6:
+        sort_list_by_length_and_alphabetically_and_reverse_and_print()
+    elif x == 7:
         break
     else:
         print("invalid option")
